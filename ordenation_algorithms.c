af17b89a73a9a2c7ebdb42d275e35e5a91e26873
Lucas Paiva <lucas.arjo22@gmail.com>
2022-05-03 22:58:19 -0300

    InsertionSort

--- ordenation_algorithms.c
+++ ordenation_algorithms.c
@@ -3,13 +3,15 @@
 
 void bubbleSort(int[], int);
 void selectionSort(int[], int);
+void insertionSort(int[], int);
 
 int main()
 {
     int vetor[] = {64, 30, 25, 12, 4, 20, 150, 300, 50, 35, 5, 700};
     int k, tam = sizeof(vetor) / sizeof(vetor[0]);
-    bubbleSort(vetor, tam);
-    // selectionSort(vetor, tam);
+    insertionSort(vetor, tam);
+    // bubbleSort(vetor, tam);
+    //  selectionSort(vetor, tam);
 
     // printf("%d", m);
 
@@ -54,5 +56,20 @@ void selectionSort(int v[], int tam)
             v[min] = v[i];
             v[i] = aux;
         }
+    }
+}
+
+void insertionSort(int v[], int tam)
+{
+    int i, j, aux;
+
+    for (i = 1; i < tam; i++)
+    {
+        aux = v[i];
+        for (j = i - 1; j >= 0 && aux < v[j]; j--)
+        {
+            v[j + 1] = v[j];
+        }
+        v[j + 1] = aux;
     }
 }
